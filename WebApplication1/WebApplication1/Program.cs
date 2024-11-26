d10fe1c773bc21d1595ece342f162e3452684ae8
Zachary Gagnon <zac08gag@gmail.com>
2024-11-26 16:24:09 -0500

    test

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -4,7 +4,7 @@ using WebApplication1.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.jhsafgjhhkjhkjhjhghjgjgj
+// Add services to the container.jhsafgjhhkjhkjhjhghjgjgjjhghjgjhghj
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
